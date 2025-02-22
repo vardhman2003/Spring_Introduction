6255b221624a00b934dbb11599f421468ad2cfd6
Vardhman Jain <vardhmanjain2003@gmail.com>
2025-02-22 12:59:02 +0530

    [Vardhman Jain] Adding UC2

--- restAPI/src/main/java/com/springIntro/restAPI/Controller/APIController.java
+++ restAPI/src/main/java/com/springIntro/restAPI/Controller/APIController.java
@@ -1,6 +1,7 @@
 package com.springIntro.restAPI.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +13,9 @@ public class APIController {
     public String hello() {
         return "Hello from BridgeLabz";
     }
+    //Make REST Call to show Hello {name} from BridgeLabz
+    @GetMapping("/hello/query")
+    public String query(@RequestParam("name") String mark){
+        return "Hello "+mark+" from Bridgelabz";
+}
 }
